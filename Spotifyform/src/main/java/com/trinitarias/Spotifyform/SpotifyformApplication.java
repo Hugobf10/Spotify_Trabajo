93c9c24fbc15130cc4c4e80e33d46dc006ef51e0
Hugo Bodon <hbodonf01@gmail.com>
2025-11-10 11:25:47 +0100

    Archivos Java

---
+++ Spotifyform/src/main/java/com/trinitarias/Spotifyform/SpotifyformApplication.java
@@ -0,0 +1,13 @@
+package com.trinitarias.Spotifyform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyformApplication.class, args);
+	}
+
+}
